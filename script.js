d8899f8d78b69498d37b15ed0445cc8a3eb09193
aleksandra <78385964+hykare@users.noreply.github.com>
2021-08-12 21:38:02 +0200

    added game function and user input, base game complete

--- script.js
+++ script.js
@@ -3,33 +3,35 @@ function computerPlay() {
     let randomInt = Math.floor(Math.random() * 3);
     return plays[randomInt];
 }
-
-const playerSelection = 'Rock';
-const computerSelection = computerPlay();
-
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase();
     if (
         (playerSelection === 'ROCK' && computerSelection === 'Scissors')
         || (playerSelection === 'PAPER' && computerSelection === 'Rock')
         || (playerSelection === 'SCISSORS' && computerSelection === 'Paper')
-    ) {
-        return 'you win!';
-    }
+    ) return 'you win!';
     else if (
         (playerSelection === 'ROCK' && computerSelection === 'Paper')
         || (playerSelection === 'PAPER' && computerSelection === 'Scissors')
         || (playerSelection === 'SCISSORS' && computerSelection === 'Rock')
-    ) {
-        return 'you lose!';
-    }
+    ) return 'you lose!';
     else return "it's a tie";
 }
 
 function game() {
-
+    let playerScore = 0;
+    let computerScore = 0;
     for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerSelection, computerSelection));
-
+        const playerSelection = prompt('choose your weapon');
+        const computerSelection = computerPlay();
+        let roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+        if (roundResult == 'you win!') playerScore++;
+        else if (roundResult == 'you lose!') computerScore++;
     }
+    if(playerScore>computerScore) console.log('PLAYER WINS');
+    else if(playerScore<computerScore) console.log('COMPUTER WINS');
+    else console.log("IT'S A TIE");
 
+}
+game();
